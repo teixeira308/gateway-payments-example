2b118ab51b4a8427082ff299ac8762a72597a19c
Guilherme Teixeira <teixeira308@hotmail.com>
2026-02-24 23:09:51 -0300

    adiciona middleware CORS para permitir requisições de diferentes origens

--- internal/interface/http/router.go
+++ internal/interface/http/router.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"gateway-payments/internal/interface/http/handler"
+	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -11,6 +12,7 @@ func NewRouter(
 	paymentHandler *handler.PaymentHandler,
 ) *chi.Mux {
 	router := chi.NewRouter()
+	router.Use(corsMiddleware)
 	router.Use(middleware.Logger)
 
 	router.Put("/payments/{id}", paymentHandler.Update)
@@ -19,4 +21,21 @@ func NewRouter(
 	router.Delete("/payments/{id}", paymentHandler.Delete)
 
 	return router
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Permite qualquer origem (ideal para desenvolvimento)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// Se for uma requisição pre-flight (OPTIONS), responde com OK e encerra
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
